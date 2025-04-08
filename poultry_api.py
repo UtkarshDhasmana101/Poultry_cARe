73f4664b866056dc064e3b5f84867996ec4655ee
UtkarshDhasmana101 <utkarshdhasmana480@gmail.com>
2025-04-08 11:53:14 +0530

    another

--- poultry_api.py
+++ poultry_api.py
@@ -40,6 +40,7 @@ def predict():
 @app.route('/features', methods=['GET'])
 def list_features():
     return jsonify({"features": features})
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
